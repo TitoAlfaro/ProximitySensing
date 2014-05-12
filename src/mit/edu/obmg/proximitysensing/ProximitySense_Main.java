89560d195a10efbb29fe2bcb812b5d6849fcfbed
TitoAlfaro <titoalfaro@gmail.com>
2014-05-12 17:36:53 -0400

    STABLE

--- src/mit/edu/obmg/proximitysensing/ProximitySense_Main.java
+++ src/mit/edu/obmg/proximitysensing/ProximitySense_Main.java
@@ -20,59 +20,61 @@ import android.widget.ToggleButton;
 
 public class ProximitySense_Main extends IOIOActivity {
 	private final String TAG = "ProximitySensing";
-	
-	//Proximity
+
+	// Proximity
 	private int sensorPin = 41;
 	private AnalogInput proximityData;
 	float distance = 1;
-	
-	//UI
+
+	// UI
 	private TextView distanceValue, _vibRate;
 	private NumberPicker minSensor, maxSensor;
 	private RadioGroup RG;
 	private RadioButton radioSonar, radioIR;
 	private boolean flagIR, flagSonar;
-	
-	//MultiThreading
+
+	// MultiThreading
 	private Thread Vibration;
 	Thread thread = new Thread(Vibration);
-	
-	//Vibration
+
+	// Vibration
 	float rate;
 	DigitalOutput out;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_proximity_sense__main);
-		
-		radioSonar = (RadioButton)findViewById(R.id.radioSonar);
-		radioSonar = (RadioButton)findViewById(R.id.radioIR);
-		
-		distanceValue = (TextView)findViewById(R.id.distance);
-		_vibRate = (TextView)findViewById(R.id.VibRate);
-		
-		String[] sensorNums = new String[31];
-	    for(int i=0; i<sensorNums.length; i++){
-			//float dec = i/10.0f;
-	    	sensorNums[i] = Integer.toString(i);
-	    }
-	    
-		minSensor = (NumberPicker)findViewById(R.id.minSensor);
-	    minSensor.setMinValue(0);
-	    minSensor.setMaxValue(30);
-	    minSensor.setWrapSelectorWheel(false);
-	    minSensor.setDisplayedValues(sensorNums);
-	    minSensor.setValue(0);
-	    minSensor.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 
-		maxSensor = (NumberPicker)findViewById(R.id.maxSensor); 
+		radioSonar = (RadioButton) findViewById(R.id.radioSonar);
+		radioSonar = (RadioButton) findViewById(R.id.radioIR);
+
+		distanceValue = (TextView) findViewById(R.id.distance);
+		_vibRate = (TextView) findViewById(R.id.VibRate);
+
+		String[] sensorNums = new String[31];
+		for (int i = 0; i < sensorNums.length; i++) {
+			// float dec = i/10.0f;
+			sensorNums[i] = Integer.toString(i);
+		}
+
+		minSensor = (NumberPicker) findViewById(R.id.minSensor);
+		minSensor.setMinValue(0);
+		minSensor.setMaxValue(30);
+		minSensor.setWrapSelectorWheel(false);
+		minSensor.setDisplayedValues(sensorNums);
+		minSensor.setValue(0);
+		minSensor
+				.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+
+		maxSensor = (NumberPicker) findViewById(R.id.maxSensor);
 		maxSensor.setMinValue(0);
 		maxSensor.setMaxValue(20);
 		maxSensor.setWrapSelectorWheel(false);
 		maxSensor.setDisplayedValues(sensorNums);
 		maxSensor.setValue(20);
-	    maxSensor.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+		maxSensor
+				.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
 	}
 
 	class Looper extends BaseIOIOLooper {
@@ -81,9 +83,9 @@ public class ProximitySense_Main extends IOIOActivity {
 
 		@Override
 		protected void setup() throws ConnectionLostException {
-			//led_ = ioio_.openDigitalOutput(0, true);
+			// led_ = ioio_.openDigitalOutput(0, true);
 			proximityData = ioio_.openAnalogInput(sensorPin);
-			
+
 			try {
 				Vibration thread_ = new Vibration(ioio_);
 				thread_.start();
@@ -96,17 +98,18 @@ public class ProximitySense_Main extends IOIOActivity {
 		@Override
 		public void loop() throws ConnectionLostException {
 			try {
-				distance = proximityData.getVoltage()*10;
+				distance = proximityData.getVoltage() * 10;
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 			}
-			
+
 			distanceValue.post(new Runnable() {
 				public void run() {
-					distanceValue.setText("Voltage: "+ distance);
+					distanceValue.setText("Voltage: " + distance);
 				}
 			});
 		}
+
 		@Override
 		public void disconnected() {
 			Log.i(TAG, "IOIO disconnected");
@@ -123,71 +126,66 @@ public class ProximitySense_Main extends IOIOActivity {
 	protected IOIOLooper createIOIOLooper() {
 		return new Looper();
 	}
-	
-	public void onRadioClick(View view){
-		//Is the button checked?
+
+	public void onRadioClick(View view) {
+		// Is the button checked?
 		boolean checked = ((RadioButton) view).isChecked();
-		
-		//which button was it?
-		switch (view.getId()){
+
+		// which button was it?
+		switch (view.getId()) {
 		case R.id.radioIR:
-			if(checked){
+			if (checked) {
 				flagIR = true;
-			//	RG.clearCheck();
+				// RG.clearCheck();
 			}
 			break;
 		case R.id.radioSonar:
-			if(checked){
+			if (checked) {
 				flagSonar = true;
-			//	RG.clearCheck();
+				// RG.clearCheck();
 			}
 			break;
-			
+
 		}
 	}
-	class Vibration extends Thread{
-    	private DigitalOutput led;
-    	
-    	private IOIO ioio_;
-    	
-    	public Vibration(IOIO ioio)throws InterruptedException{
-    		ioio_ = ioio;
-    	}
-    	
-    	public void run(){
-    		super.run();
+
+	class Vibration extends Thread {
+		private DigitalOutput led;
+
+		private IOIO ioio_;
+
+		public Vibration(IOIO ioio) throws InterruptedException {
+			ioio_ = ioio;
+		}
+
+		public void run() {
+			super.run();
 			while (true) {
 				try {
 					led = ioio_.openDigitalOutput(0, true);
-					out = ioio_.openDigitalOutput(13,false);
+					out = ioio_.openDigitalOutput(13, false);
 					while (true) {
-						if (distance < minSensor.getValue()){
-							rate = 1000;
-						}else if (distance > maxSensor.getValue()){
-							rate = 5;
-						}else{
-							if(flagIR){
-								Log.i(TAG, "IR");
-								rate = map(distance, 	(float) 0,//minSensor.getValue(), 
-														(float) 20,//maxSensor.getValue(), 
-														(float) 1000, 
-														(float) 5);
-								Log.i(TAG, "minSensor/10 = "+ minSensor.getValue());
-							}else{
-								Log.i(TAG, "Sonar");
-								rate = map(distance, 	(float) 0,//minSensor.getValue(), 
-														(float) 20,//maxSensor.getValue(), 
-														(float) 5, 
-														(float) 1000);
-							}
+
+						if (flagIR) {
+							Log.i(TAG, "IR");
+							rate = map(distance, (float) 0,// minSensor.getValue(),
+									(float) 20,// maxSensor.getValue(),
+									(float) 1000, (float) 5);
+							Log.i(TAG, "minSensor/10 = " + minSensor.getValue());
+						} else {
+							Log.i(TAG, "Sonar");
+							rate = map(distance * 10,
+									(float) 2,// minSensor.getValue(),
+									(float) maxSensor.getValue(), (float) 5,
+									(float) 1000);
 						}
-						
+
 						_vibRate.post(new Runnable() {
 							public void run() {
-								_vibRate.setText("Rate: "+ rate);
+								_vibRate.setText("Rate: " + rate);
 							}
 						});
-						
+
 						led.write(false);
 						out.write(true);
 						sleep((long) 50);
@@ -207,11 +205,16 @@ public class ProximitySense_Main extends IOIOActivity {
 					}
 				}
 			}
-    	}
-    }
-	
-	float map(float x, float in_min, float in_max, float out_min, float out_max)
-	{
-	  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+		}
+	}
+
+	float map(float x, float in_min, float in_max, float out_min, float out_max) {
+		if (x < in_min)
+			return out_min;
+		else if (x > in_max)
+			return out_max;
+		else
+			return (x - in_min) * (out_max - out_min) / (in_max - in_min)
+					+ out_min;
 	}
 }
